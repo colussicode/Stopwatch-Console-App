39314c30f433ac569f8116409a69660f97badeb8
Joao Colussi <colussicode@gmail.com>
2020-12-09 20:24:47 -0300

    Started to create some functions

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Escolha uma opção de contagem: ");
+            Console.WriteLine("S - Segundos");
+            Console.WriteLine("M - Minutos");
+            Console.WriteLine("Quanto tempo deseja cronometrar?");
+            Console.WriteLine("0 - Sair");
+
+            string data = Console.ReadLine().ToLower();
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while(currentTime <= time) {
+                Console.Clear();
+                Console.WriteLine(currentTime);
+                currentTime ++;
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado...Retornando para o menu!");
+            Thread.Sleep(2000);
+        }
+    }
+}
